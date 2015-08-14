cdf33746c55f6d3fc9357a88052c8f73463e9a55
Mikael Falkvidd git@mjo.se <git@mjo.se>
2015-08-14 18:18:34 +0200

    Measure battery level

--- energyreader.ino
+++ energyreader.ino
@@ -16,6 +16,7 @@ MySensor gw;
 #define LOW_PRESET_PIN 7
 #define LDR_ANALOG_PIN 14
 #define mydebug true
+#define BATTERYFULL 30.0 // 3,000 millivolts * 100 to get percentage
 volatile unsigned int ledblinks = 0;
 unsigned int preset_resistance = 1000;
 unsigned int mainLoopCounter = 0;
@@ -25,6 +26,12 @@ unsigned int blinksSinceLast = 0;
 unsigned int batteryLevelPercent = 88; // TODO: Measure and set
 unsigned long watts = 0;
 
+void updateBatteryLevelPercent() {
+  log("Battery level: ");
+  batteryLevelPercent = readVcc() / BATTERYFULL;
+  log(batteryLevelPercent);
+  log("\n");
+}
 
 void calibrate()
 {
@@ -108,11 +115,39 @@ void log(String text) {
   }
 }
 
+long readVcc() {
+  // From http://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+#if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+  ADMUX = _BV(MUX5) | _BV(MUX0);
+#elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+  ADMUX = _BV(MUX3) | _BV(MUX2);
+#else
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+#endif
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA, ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high << 8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
 void loop()
 {
   detachInterrupt(BLINK_INTERRUPT);
   unsigned long timeNow = millis();
   gw.process();
+
   blinksSinceLast = ledblinks;
   ledblinks = 0;
 
@@ -158,6 +193,7 @@ void sendResults() {
   if (!gw.send(msgResistor.set(preset_resistance, 1))) {
     log("No ACK for resistance\n");
   }
+  updateBatteryLevelPercent();
   gw.sendBatteryLevel(batteryLevelPercent);
 }
 
